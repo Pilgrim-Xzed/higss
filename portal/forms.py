2751287669100d3ef2b2637f6bad47c4dbe627ec
Pilgrim001 <saeedbello12@gmail.com>
2019-07-29 10:19:54 +0100

    Fixed some errors

--- portal/forms.py
+++ portal/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm,DateField,DateInput,FileInput
 from django import forms
 from .models import Application
+from django.utils.translation import ugettext_lazy as _
 
 class ApplicationForm(ModelForm):
     class Meta:
@@ -9,6 +10,17 @@ class ApplicationForm(ModelForm):
         widgets = {
             'date_of_birth':DateInput(attrs={'type':'date','class':'datetimepicker-dummy-input','readonly':'readonly'}),
             'passport':FileInput(attrs={'class':'file-input'})
+        }
+        labels = {
+            'passport':_("Pupil's Picture"),
+            'firstname_of_pupil':_("First Name of Pupil"),
+            'surname_of_pupil':_("Surname of Pupil"),
+            'othername_of_pupil':_("Middle Name of Pupil"),
+            'sex':_("Gender"),
+            'date_of_birth':_("Date of Birth"),
+            'class_applying_to':_("Class Applying to"),
+            'full_name_of_guardian':_("Full Name of Sponsor"),
+            'relationship_with_pupil':_("Relationship with Pupil")
         }
 
         exclude = ('user', 'passed', 'timestamp', 'admission_number')
